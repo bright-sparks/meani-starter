b8d4ed37f6d566f3b7bfb06673d723806bc36447
Caner Çakmak <canercak@gmail.com>
2015-09-03 23:25:25 +0300

    change grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -465,28 +465,7 @@ module.exports = function (grunt) {
         dest: 'dist-ionic/www',
         expand: true
       }
-    },
-
-    connect: {
-      options: {
-        port: 9000,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost'
-      },
-      dist: {
-        options: {
-          base: '<%= yeoman.dist %>'
-        }
-      },
-      coverage: {
-        options: {
-          port: 9002,
-          open: true,
-          base: ['coverage']
-        }
-      }
-    },
-
+    }, 
     buildcontrol: {
       options: {
         dir: 'dist',
